0361cb924b21d86aabf8e3c1100680fd8b5ef893
yuikanaz <osaka.hakuba.18@gmail.com>
2025-03-10 01:28:07 +0900

    add limit to new task

--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -10,16 +10,19 @@ export const NewTask = () => {
   const [selectListId, setSelectListId] = useState()
   const [lists, setLists] = useState([])
   const [title, setTitle] = useState('')
+  const [limit, setLimit] = useState('')
   const [detail, setDetail] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
   const [cookies] = useCookies()
   const navigate = useNavigate()
   const handleTitleChange = (e) => setTitle(e.target.value)
+  const handleLimitChange = (e) => setLimit(e.target.value)
   const handleDetailChange = (e) => setDetail(e.target.value)
   const handleSelectList = (id) => setSelectListId(id)
   const onCreateTask = () => {
     const data = {
       title: title,
+      limit: limit,
       detail: detail,
       done: false,
     }
@@ -80,6 +83,14 @@ export const NewTask = () => {
             type="text"
             onChange={handleTitleChange}
             className="new-task-title"
+          />
+          <br />
+          <label>期限</label>
+          <br />
+          <input
+            type="datetime-local"
+            onChange={handleLimitChange}
+            className="new-task-limit"
           />
           <br />
           <label>詳細</label>
